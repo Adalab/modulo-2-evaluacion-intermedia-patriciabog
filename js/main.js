5b02096d1d53cb55c9c430bd5d338d9afe84826c
Patricia <patriciamvbog@gmail.com>
2023-02-18 02:50:53 +0000

    Optimized functions and added a button reset

--- js/main.js
+++ js/main.js
@@ -1,51 +1,71 @@
 'use strict';
-
+//variables
 const inputNumber = document.querySelector ('.js-inputNumber');
 const button = document.querySelector ('.js-button');
 const text = document.querySelector('.js-text');
-const counter = document.querySelector('.js-try')
+const counter = document.querySelector('.js-counter');
+const btnReset = document.querySelector('.js-reset');
 
 const randomNumber = getRandomNumber(100);
-
+//console.log(randomNumber);
 let cont = 0;
 
+// Function random number
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-
+//Optinized function 
 function addNum() {
   const num = parseInt(inputNumber.value);
-  console.log("Input number: " + num);
-    
-  if (num < 1 || num > 100) {
-    text.innerHTML = "El número debe estar entre 1 y 100";
+  //console.log("Input number: " + num);
+  if (isNaN(num)) {
+    showMessage('Debe introducir un número');
+  } else if (num < 1 || num > 100) {
+    showMessage("El número debe estar entre 1 y 100");
   } else if (num > randomNumber) {
-    text.innerHTML = "Pista: Demasiado alto";
-  }else if (num < randomNumber) {
-    text.innerHTML = "Pista: Demasiado bajo";
+    showMessage("Pista: Demasiado alto");
+  } else if (num < randomNumber) {
+    showMessage( "Pista: Demasiado bajo");
   }  else if (num === randomNumber) {
-    text.innerHTML = "Has ganado campeona!!!";
-  }else {
-    text.innerHTML = "El número debe estar entre 1 y 100";
+   showMessage("Has ganado campeona!!!");
+  } else {
+    showMessage("El número debe estar entre 1 y 100");
   }
 }
 
-function increaseCounter() {
- cont += 1;
- counter.value = `Número de intentos: ${cont}`;
+function showMessage (message) {
+  text.innerHTML = message;
 }
 
 
-function handleClickButton() {
+// Function show counter
+const increaseCounter = () => {
+   cont++
+   counter.innerHTML = `Número de intentos: ${cont}`;
+}
 
+
+//Function event
+function handleClickButton() {
  addNum();
  increaseCounter();
 }
 
+//Function for Reset
+const handleClickReset = (ev) => {
+  showMessage("Escribe un número y dale a Prueba");
+  cont = 0;
+  counter.innerHTML = `Número de intentos: ${cont}`;
+};
+
 
 console.log("Random number: " + randomNumber);
+
+// Event Click
 button.addEventListener("click", handleClickButton);
+btnReset.addEventListener('click', handleClickReset);
+
 
 
 
